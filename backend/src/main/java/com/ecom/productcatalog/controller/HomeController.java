78977fb7247cd127e4363abeefc1a1881facff5c
jinka <jinkalkers@gmail.com>
2026-05-16 18:50:38 +0530

    Initial backend spring boot project

---
+++ backend/src/main/java/com/ecom/productcatalog/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.ecom.productcatalog.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Product Catalog API is running!";
+    }
+}
